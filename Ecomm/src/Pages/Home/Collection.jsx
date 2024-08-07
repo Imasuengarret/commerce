7cb21c4081da2f08226db56238c98f9fd5df5fcb
Imasuengarret <garretharry@gmail.com>
2024-08-07 15:59:15 +0100

    updated collection & Newsletter files

--- Ecomm/src/Pages/Home/Collection.jsx
+++ Ecomm/src/Pages/Home/Collection.jsx
@@ -7,7 +7,7 @@ function Collection() {
       <div className='h-[550px] flex justify-between md:flex-row items-center'>
             <div className='md:w-1/2'></div>
             <div className='md:w-1/2'>
-                <img src="/public/images/zara-logo.png" alt="" className=''/>
+                <img src="/images/zara-logo.png" alt="" className=''/>
                 <p className='text-lg text-white my-8 md:w-2/3 leading-[32px]'>Lustrous yet understated. The New Evening Wear Collection Exclusively Offered At The Reopened Giorgio Armani Boutique in Los Angeles </p>
                 <button className='px-6 py-2 bg-white text-black rounded-sm capitalize font-semibold'>see collections</button>
             </div>
--- Ecomm/src/Pages/Home/NewsLetters.jsx
+++ Ecomm/src/Pages/Home/NewsLetters.jsx
@@ -9,22 +9,22 @@ function NewsLetters() {
         {/* insta grid */}
         <div className='flex lg:flex-row flex-col gap-4 items-center justify-center mb-20'>
             <Link to="/">
-                <img src="/public/images/instagram/IMG1.png" alt="" />
+                <img src="/images/Instagram/IMG1.png" alt="" />
             </Link>
             <Link to="/">
-                <img src="/public/images/instagram/IMG2.png" alt="" />
+                <img src="/images/Instagram/IMG2.png" alt="" />
             </Link>
             <Link to="/">
-                <img src="/public/images/instagram/IMG3.png" alt="" />
+                <img src="/images/Instagram/IMG3.png" alt="" />
             </Link>
             <Link to="/">
-                <img src="/public/images/instagram/IMG4.png" alt="" />
+                <img src="/images/Instagram/IMG4.png" alt="" />
             </Link>
             <Link to="/">
-                <img src="/public/images/instagram/IMG5.png" alt="" />
+                <img src="/images/Instagram/IMG5.png" alt="" />
             </Link>
             <Link to="/">
-                <img src="/public/images/instagram/IMG6.png" alt="" />
+                <img src="/images/Instagram/IMG6.png" alt="" />
             </Link>
         </div>
 
@@ -33,7 +33,7 @@ function NewsLetters() {
             <h2 className='title mb-8'>or subscribe to the newsletter</h2>
             <form className='md:w-1/2 mx-auto text-center'>
                 <input type="email" name='email' id='email' placeholder='Email address...' className='h-8 bg-transparent outline-none border-b-2 pl-2 border-black md:w-2/3 w-full mb-5 placeholder:text-black/75 mr-4' />
-                <input type="submit" value={"submit"}  className='bg-black text-white rounded-sm py-1 px-6'/>
+                <input type="submit" value={"submit"}  className='bg-black text-white rounded-sm py-2 px-6'/>
             </form>
         </div>
 
